2c5a899ee25b6afd72e126134b5adafb7379d9af
Adithya <adithyav102@gmail.com>
2026-02-20 06:54:33 +0530

    Add cors

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 3001;
 
 // Configure CORS
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173', 'https://runtime-terrors-api.onrender.com'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173', 'https://runtime-terrors-api.onrender.com', 'https://forensiq-eta.vercel.app/'],
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type']
 }));
